019f65218cd6bece2b874ad500db05e2264e2d9e
Humza <109144839+humzaco@users.noreply.github.com>
2023-04-06 14:51:11 +0500

    add restart function and  player life

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 import showBanner from "node-banner";
 (async () => {
-    await showBanner('Guess Game', 'Start Game', 'green');
+    await showBanner("Guess Game", "Start Game", "green");
 })();
-let playerLife = 3;
+let playerLife = 4;
 async function askQusestion() {
     let randomNum = Math.ceil(Math.random() * 10 + 1);
     do {
         playerLife--;
-        var que = await inquirer
-            .prompt([
+        console.log(chalk.rgb(251, 151, 63)(`Player life left ${playerLife}`));
+        var que = await inquirer.prompt([
             {
                 type: "number",
                 name: "user_num",
-                message: "Select any number between 1 - 10: ",
-            }
+                message: chalk.rgb(96, 35, 16)("Select any number between 1 - 10: "),
+            },
         ]);
         if (que.user_num === randomNum) {
             console.log(chalk.green(`Congratulation!! Your guess the right number`));
@@ -27,7 +27,27 @@ async function askQusestion() {
             console.log(chalk.red(`Your number ${que.user_num} is greater than guess number.`));
         }
     } while (playerLife > 0 && randomNum !== que.user_num);
+    if (playerLife == 0) {
+        console.log(chalk.redBright(`GAME OVER!!`));
+    }
+}
+async function stratAgain() {
+    do {
+        console.clear();
+        playerLife = 4;
+        await askQusestion();
+        var restart = await inquirer.prompt([
+            {
+                type: "input",
+                name: "start_again",
+                message: chalk.rgb(233, 58, 4)("Do you want to restart the game? Press Y or N: "),
+            },
+        ]);
+    } while (restart.start_again === "y" ||
+        restart.start_again === "Y" ||
+        restart.start_again === "yes" ||
+        restart.start_again === "YES");
 }
 setTimeout(() => {
-    askQusestion();
+    stratAgain();
 }, 1000);
--- index.ts
+++ index.ts
@@ -4,39 +4,62 @@ import chalkAnimation from "chalk-animation";
 import showBanner from "node-banner";
 
 (async () => {
-    await showBanner('Guess Game', 'Start Game', 'green');
+  await showBanner("Guess Game", "Start Game", "green");
 })();
 
-
-let playerLife = 3;
+let playerLife = 4;
 
 async function askQusestion() {
-    let randomNum:number = Math.ceil(Math.random() * 10 + 1);
-    do{
-        playerLife--;
-    var que = await inquirer
-    .prompt([
-        {
-            type: "number",
-            name: "user_num",
-            message: "Select any number between 1 - 10: ",
-        }
+  let randomNum: number = Math.ceil(Math.random() * 10 + 1);
+  do {
+    playerLife--;
+    console.log(chalk.rgb(251, 151, 63)(`Player life left ${playerLife}`));
+    var que = await inquirer.prompt([
+      {
+        type: "number",
+        name: "user_num",
+        message: chalk.rgb(96, 35, 16)("Select any number between 1 - 10: "),
+      },
     ]);
 
-    if(que.user_num === randomNum){
-        console.log(chalk.green(`Congratulation!! Your guess the right number`));
-    } else if(que.user_num < randomNum){
-        console.log(chalk.red(`Your number ${que.user_num} is less than guess number.`));
-    }else if(que.user_num > randomNum){
-        console.log(chalk.red(`Your number ${que.user_num} is greater than guess number.`));
+    if (que.user_num === randomNum) {
+      console.log(chalk.green(`Congratulation!! Your guess the right number`));
+    } else if (que.user_num < randomNum) {
+      console.log(
+        chalk.red(`Your number ${que.user_num} is less than guess number.`)
+      );
+    } else if (que.user_num > randomNum) {
+      console.log(
+        chalk.red(`Your number ${que.user_num} is greater than guess number.`)
+      );
     }
-    }while(playerLife > 0 && randomNum !== que.user_num)
+  } while (playerLife > 0 && randomNum !== que.user_num);
+
+  if (playerLife == 0) {
+    console.log(chalk.redBright(`GAME OVER!!`));
+  }
 }
 
-setTimeout(()=>{
-    askQusestion()
-}, 1000)
+async function stratAgain() {
+  do {
+    console.clear();
+    playerLife = 4;
+    await askQusestion();
+    var restart = await inquirer.prompt([
+      {
+        type: "input",
+        name: "start_again",
+        message: chalk.rgb(233, 58, 4)("Do you want to restart the game? Press Y or N: "),
+      },
+    ]);
+  } while (
+    restart.start_again === "y" ||
+    restart.start_again === "Y" ||
+    restart.start_again === "yes" ||
+    restart.start_again === "YES"
+  );
+}
 
-
-
-
+setTimeout(() => {
+  stratAgain();
+}, 1000);
